0dee184d0e0d42c720c282cc47fec2373919daa7
akhil-2005 <141239702+akhil-2005@users.noreply.github.com>
2023-08-29 20:51:13 +0530

    Add files via upload

---
+++ garbage.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+main()
+{
+	
+	int a;
+	printf("a=%d\n",a);//the opput will be garbage value , a grabagebnvalue is that one which'
+	a=10;
+	printf("a is %d\n",a);
+}
---
+++ variabledeclaration.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+main()
+{
+	int num1;
+	int num2;
+	
+	num1=100;
+	printf("enter the second number:");
+	scanf("%d",&num2);
+	printf("num1= %d \n num2=%d",num1,num2);
+	
+}
